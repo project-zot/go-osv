4adfb2d21aee48f46afa03698395b1a29195f8f3
Ramkumar Chinchani <rchincha@cisco.com>
2022-11-13 02:45:34 +0000

    feat: add download dir
    Signed-off-by: Ramkumar Chinchani <rchincha@cisco.com>

--- pkg/osv/constants.go
+++ pkg/osv/constants.go
@@ -7,27 +7,29 @@ const (
 	Android Ecosystem = "Android"
 )
 
-/*
-"Alpine"
-"Android"
-"crates.io"
-"Debian"
-"DWF"
-"GitHub Actions"
-"Go"
-"GSD"
-"Hex"
-"Linux"
-"Maven"
-"npm"
-"NuGet"
-"OSS-Fuzz"
-"Packagist"
-"Pub"
-"PyPI"
-"RubyGems"
-"UVI"
-*/
+func GetAllEcosystems() []string {
+	return []string{
+		"Alpine",
+		"Android",
+		"crates.io",
+		"Debian",
+		"DWF",
+		"GitHub Actions",
+		"Go",
+		"GSD",
+		"Hex",
+		"Linux",
+		"Maven",
+		"npm",
+		"NuGet",
+		"OSS-Fuzz",
+		"Packagist",
+		"Pub",
+		"PyPI",
+		"RubyGems",
+		"UVI",
+	}
+}
 
 func (e *Ecosystem) Lookup() string {
 	return ""
